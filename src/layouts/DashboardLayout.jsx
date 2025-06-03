2b92ca1dc5a4b559a1afd6094b73be425776fff7
Ejiofor Joshua <ejioforjo01@gmail.com>
2025-06-03 01:17:10 +0100

    logo changes

--- src/layouts/DashboardLayout.jsx
+++ src/layouts/DashboardLayout.jsx
@@ -1,7 +1,7 @@
 import React, { useState, useEffect } from 'react'
 import { Outlet, NavLink, useNavigate } from 'react-router-dom'
-import logo from './assets/fullLogo.png'
-import icon from './assets/icon.png'
+import logo from '../assets/fullLogo.png'
+import icon from '../assets/icon.png'
 
 const DashboardLayout = () => {
   const [isMobile, setIsMobile] = useState(window.innerWidth < 768)
